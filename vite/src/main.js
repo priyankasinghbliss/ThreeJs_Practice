f43ade1740ae3d7ef493ee06797d8f8ba7ad0d97
priyankasinghbliss <priyanka.23.12.1999@gmail.com>
2025-06-14 00:59:09 +0530

    Vite using Vanilla JS

---
+++ vite/src/main.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const canvas = document.getElementById('canvas');
+
+// 1. Create a scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Create an Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry( 2, 0.1, 2);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: '#B4B4B3', emissive: '#B4B4B3' });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// 4. Add lights
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio); // Usefull for mobile devices so controls also work there.
+
+// 6. Add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. Add animations
+function animate() {
+  requestAnimationFrame(animate);
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.005;
+  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+  renderer.render(scene, camera);
+}
+
+animate();
+
+// 8. Handle window resize
+window.addEventListener('resize',() => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
